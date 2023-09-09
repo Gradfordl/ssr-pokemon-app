afe41540bcf2e4616024c37600d87b7d6411b274
gradfordl <laurengradford@gmail.com>
2023-09-08 20:17:20 -0400

    "Created show views of each pokemon"

--- views/Index.jsx
+++ views/Index.jsx
@@ -9,12 +9,14 @@ class Index extends React.Component {
     const { pokemon } = this.props;
     return (
       <div style={myStyle}>
-        <h1>Index Page</h1>
+        <h1>See All The Pokemon</h1>
         <ul>
           {pokemon.map((pokemon, i) => {
             return (
               <li key={i}>
-                <a href={`/pokemon/${i}`}>{pokemon.name}</a>
+                <a href={`/pokemon/${i}`}>
+                    {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
+                </a>
                 <br />
               </li>
             );
